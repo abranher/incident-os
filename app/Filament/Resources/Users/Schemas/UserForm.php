99e9a0c29adc6f6d72b372d6f50de20207b82792
Abraham Hernandez <abrahamycaldera@gmail.com>
2026-01-30 23:18:00 -0400

    chore: install and configure laravel-lang/common package & feat: add user resource.

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Support\Enums\Operation;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+  public static function configure(Schema $schema): Schema
+  {
+    return $schema
+        ->components([
+          TextInput::make('name')
+            ->label('Nombre')
+            ->required(),
+          TextInput::make('email')
+            ->label('Correo electrónico')
+            ->unique()
+            ->email()
+            ->required(),
+          TextInput::make('password')
+            ->label('Contraseña')
+            ->password()
+            ->rule('confirmed')
+            ->required()
+            ->revealable()
+            ->hiddenOn(Operation::Edit),
+          TextInput::make('password_confirmation')
+            ->label('Confirmar contraseña')
+            ->password()
+            ->required()
+            ->revealable()
+            ->hiddenOn(Operation::Edit),
+        ]);
+  }
+}
